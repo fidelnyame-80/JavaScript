6887262be4a493e9fdd6631d91f435a06310a85b
fidelnyame-80 <nyamefidel7@gmail.com>
2025-08-14 17:41:49 +0000

    jsweek4

---
+++ jsweek4.js
@@ -0,0 +1,47 @@
+let car = {
+    brand: "Toyota",
+    model: "Corolla",
+    year: 2020
+}
+
+console.log(`My car is a ${car.brand} ${car.model}`)
+
+// const displaySentence = document.createElement('div');
+// const 
+
+
+let book = {
+    title: "javaScript Basics",
+    pages: 150
+}
+
+book.pages = 200;
+
+console.log(book.pages);
+
+let student = {
+    name: "Ama",
+    age: 21,
+}
+
+student.course = "Computer Science";
+console.log(student);
+
+
+let phone = {
+    brand: "Samsung",
+    model: "Galaxy S21",
+    price: 500
+}
+
+delete phone.price 
+console.log(phone)
+
+let person = {
+    name: "Kwame",
+    hobbies: ["Swimming","Gaming","Singing"],
+
+}
+
+console.log(person.hobbies[1]);
+
